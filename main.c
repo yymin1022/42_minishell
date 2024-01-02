2aa91445da56e806d8c4b6f68a70d60cd80dfea7
yonyoo <yonyoo@student.42seoul.kr>
2024-01-02 17:51:59 +0900

    Update minishell

--- main.c
+++ main.c
@@ -6,20 +6,38 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 16:02:04 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/01/02 17:45:33 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/01/02 17:51:45 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pm_shell.h"
 
+void	check_leak()
+{
+	system("leaks minishell");
+}
+
+size_t	ft_strcmp(char *src, char *dst)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] && dst[i] && src[i] == dst[i])
+		i++;
+	return (src[i] - dst[i]);
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	char	*input;
 
+	atexit(check_leak);
 	while (1)
 	{
 		input = readline("pmshell> :$ ");
 		printf("Wa Sans %s\n", input);
+		if (!ft_strcmp(input, "exit"))
+			exit (0);
 		add_history(input);
 		free(input);
 	}
