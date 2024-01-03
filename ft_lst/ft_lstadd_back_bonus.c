7c70dd4848d21946ec28703caab1329748bc7ce1
falconlee236 <falconlee236@cau.ac.kr>
2024-01-03 15:32:53 +0900

    add ft_lst directory

---
+++ ft_lst/ft_lstadd_back_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangylee <sangylee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/18 12:02:31 by sangylee          #+#    #+#             */
+/*   Updated: 2023/03/19 21:15:19 by sangylee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*node;
+
+	if (*lst)
+	{
+		node = *lst;
+		while (node->next)
+			node = node->next;
+		node->next = new;
+	}
+	else
+		*lst = new;
+}
