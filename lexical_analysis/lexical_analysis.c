3ff8fe0f3df9e5167f390e6c4df16feb731527c9
falconlee236 <falconlee236@cau.ac.kr>
2024-01-07 07:16:46 +0900

    heredoc clear

--- lexical_analysis/lexical_analysis.c
+++ lexical_analysis/lexical_analysis.c
@@ -6,7 +6,7 @@
 /*   By: sangylee <sangylee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 14:44:59 by sangylee          #+#    #+#             */
-/*   Updated: 2024/01/04 22:00:07 by sangylee         ###   ########.fr       */
+/*   Updated: 2024/01/07 07:16:35 by sangylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	print_list(t_token *token_list)
 	printf("-----------------\n");
 }
 
-int	handle_quote(t_token **token_list, char *s, int start)
+int	handle_quote(t_token *token_list, char *s, int start)
 {
 	int		offset;
 	char	*str;
@@ -34,12 +34,11 @@ int	handle_quote(t_token **token_list, char *s, int start)
 		str = ft_strdup(s + start);
 	else
 		str = ft_substr(s, start + 1, offset - 1);
-	token_pushback(token_list, token_createnew(str, TOKEN_TYPE_ARGV));
-	free(str);
+	token_pushback(&token_list, token_createnew(str, TOKEN_TYPE_ARGV));
 	return (start + offset + 1);
 }
 
-int	handle_limiter(t_token **token_list, char *s, int start)
+int	handle_limiter(t_token *token_list, char *s, int start)
 {
 	int		offset;
 	char	*str;
@@ -52,60 +51,52 @@ int	handle_limiter(t_token **token_list, char *s, int start)
 		{
 			offset = 1;
 			while (s[start + offset] && !is_tokenable_sep(s[start + offset])
-				&& s[start + offset] == '\''
-				&& s[start + offset] == '\"')
+				&& s[start + offset] != '\''
+				&& s[start + offset] != '\"')
 					offset++;
 			str = ft_substr(s, start, offset);
-			token_pushback(token_list, token_createnew(str, TOKEN_TYPE_ARGV));
-			free(str);
+			token_pushback(&token_list, token_createnew(str, TOKEN_TYPE_ARGV));
 			start += offset;
 		}
 	}
 	return (start);
 }
 
-void	handle_heredoc_with_limiter(t_token *cur, t_token **token_list)
+void	handle_heredoc_with_limiter(t_token *token_list)
 {
 	int		start_idx;
 	char	*token_str;
 	char	*str;
 
-	token_str = cur->str;
+	token_str = ft_strdup(token_list->str);
 	start_idx = 0;
 	while (token_str[start_idx] && ft_strncmp(token_str + start_idx, "<<", 2))
 		start_idx++;
 	if (token_str[start_idx] == '\0')
 		return ;
 	start_idx += 2;
-	str = ft_substr(token_str, 0, start_idx);
-	token_pushback(token_list, token_createnew(str, TOKEN_TYPE_CHUNK));
+	free(token_list->str);
+	token_list->str = ft_substr(token_str, 0, start_idx);
 	while (token_str[start_idx] && token_str[start_idx] == ' ')
 		start_idx++;
 	start_idx = handle_limiter(token_list, token_str, start_idx);
 	if (ft_strlen(token_str) - start_idx == 0)
 	{
-		token_list_delfront(token_list);
+		free(token_str);
 		return ;
 	}
 	str = ft_substr(token_str, start_idx, ft_strlen(token_str) - start_idx);
-	token_pushback(token_list, token_createnew(str, TOKEN_TYPE_CHUNK));
-	token_list_delfront(token_list);
+	token_pushback(&token_list, token_createnew(str, TOKEN_TYPE_CHUNK));
+	free(token_str);
 }
 
-void	handle_heredoc(t_token **token_list)
+void	handle_heredoc(t_token *token_list)
 {
-	t_token	*cur;
-
-	cur = *token_list;
-	while (cur)
+	while (token_list)
 	{
-		if (cur->type == TOKEN_TYPE_CHUNK)
-		{
-			handle_heredoc_with_limiter(cur, token_list);
-			cur = (*token_list)->next;
-		}
-		else
-			cur = cur->next;
+		if (token_list->type == TOKEN_TYPE_CHUNK)
+			handle_heredoc_with_limiter(token_list);
+		token_list = token_list->next;
 	}
 }
 
@@ -114,6 +105,6 @@ void	lexical_analysis(char *s)
 	t_token	*token_list;
 
 	token_list = token_createnew(ft_strdup(s), TOKEN_TYPE_CHUNK);
-	handle_heredoc(&token_list);
+	handle_heredoc(token_list);
 	print_list(token_list);
 }
