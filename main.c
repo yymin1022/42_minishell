d13ea36859af4005c6035c5d67a13601fbb957bc
falconlee236 <falconlee236@cau.ac.kr>
2024-01-15 13:37:47 +0900

    Update pm_shell: add while statement in main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sangylee <sangylee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 16:02:04 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/01/15 13:27:51 by sangylee         ###   ########.fr       */
+/*   Updated: 2024/01/15 13:35:30 by sangylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ void	print_cmd_list(t_cmd *cmd_list)
 		i = 0;
 		tmp = cmd_list->redirect;
 		printf("argv\n");
-		while (cmd_list->argv[i])
+		while (cmd_list->argv && cmd_list->argv[i])
 			printf("%s ", cmd_list->argv[i++]);
 		printf("\nredirect\n");
 		while (tmp)
@@ -71,39 +71,42 @@ int	main(int argc, char **argv, char **env)
 	(void)argc;
 	(void)argv;
 	init_info(&info, env);
-	tmp = readline("pmshell> :$ ");
-	input = ft_strtrim(tmp, " ");
-	free(tmp);
-	if (!ft_strcmp(input, "exit") || !input)
-		exit (0);
-	add_history(input);
-	token_list = lexical_analysis(&info, input);
-	free(input);
-	if (!syntax_analysis(token_list))
+	while (!info.is_error)
 	{
-		info.is_error = 1;
-		ft_putstr_fd("pm_shell: invalid syntax\n", STDERR_FILENO);
+		tmp = readline("pmshell> :$ ");
+		input = ft_strtrim(tmp, " ");
+		free(tmp);
+		if (!ft_strcmp(input, "exit") || !input)
+			exit (0);
+		add_history(input);
+		token_list = lexical_analysis(&info, input);
+		free(input);
+		if (!syntax_analysis(token_list))
+		{
+			info.is_error = 1;
+			ft_putstr_fd("pm_shell: invalid syntax\n", STDERR_FILENO);
+			token_listclear(&token_list);
+			env_listclear(&(info.env_list));
+			return (0);
+		}
+		print_list(token_list);
+		cmd_list = make_cmdlist(token_list);
+		print_cmd_list(cmd_list);
 		token_listclear(&token_list);
 		env_listclear(&(info.env_list));
-		return (0);
+		cmd_listclear(&cmd_list);
 	}
-	print_list(token_list);
-	cmd_list = make_cmdlist(token_list);
-	print_cmd_list(cmd_list);
-	token_listclear(&token_list);
-	env_listclear(&(info.env_list));
-	cmd_listclear(&cmd_list);
 	return (0);
 }
 
 /*
-	// while (!info.is_error)
-	// {
-	// 	input = readline("pmshell> :$ ");
-	// 	if (!ft_strcmp(input, "exit"))
-	// 		exit (0);
-	// 	add_history(input);
-	// 	lexical_analysis(&info, input);
-	// 	free(input);
-	// }
+	while (!info.is_error)
+	{
+		input = readline("pmshell> :$ ");
+		if (!ft_strcmp(input, "exit"))
+			exit (0);
+		add_history(input);
+		lexical_analysis(&info, input);
+		free(input);
+	}
 */
