8de9ea6ac207baf5d3f5a8022887c3b67cfacc90
falconlee236 <falconlee236@cau.ac.kr>
2024-01-20 14:37:45 +0900

    update pm_shell: handle segment fault with ft_strtrim

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sangylee <sangylee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 16:02:04 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/01/15 13:35:30 by sangylee         ###   ########.fr       */
+/*   Updated: 2024/01/20 14:36:29 by sangylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,14 @@ int	main(int argc, char **argv, char **env)
 	{
 		tmp = readline("pmshell> :$ ");
 		input = ft_strtrim(tmp, " ");
+		if (input[0] == '\0')
+		{
+			four_times_free(tmp, input, 0, 0);
+			info.is_error = 1;
+			ft_putstr_fd("pm_shell: invalid syntax\n", STDERR_FILENO);
+			env_listclear(&(info.env_list));
+			continue ;
+		}
 		free(tmp);
 		if (!ft_strcmp(input, "exit") || !input)
 			exit (0);
@@ -87,7 +95,7 @@ int	main(int argc, char **argv, char **env)
 			ft_putstr_fd("pm_shell: invalid syntax\n", STDERR_FILENO);
 			token_listclear(&token_list);
 			env_listclear(&(info.env_list));
-			return (0);
+			continue ;
 		}
 		print_list(token_list);
 		cmd_list = make_cmdlist(token_list);
