f8aa3e014ef84d19e2fc41c9f7baca317ad83a29
yonyoo <yonyoo@student.42seoul.kr>
2024-01-21 15:53:02 +0900

    Update sig_handle : Added Handle of SIGINT

--- sig_handle/sig_handle.c
+++ sig_handle/sig_handle.c
@@ -6,34 +6,23 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 20:21:47 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/01/20 21:49:52 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/01/21 15:51:44 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pm_shell.h"
 
-static void	sig_handle(int sig)
+static void	sig_int_handle(int sig)
 {
-	signal(SIGINT, SIG_IGN);
-	signal(SIGQUIT, SIG_IGN);
-	signal(SIGTERM, SIG_IGN);
-	if (sig == SIGINT)
-	{
-		printf("Ctrl + C (SIGINT)\n");
-	}
-	else if (sig == SIGTERM)
-	{
-		printf("Ctrl + D (SIGTERM)\n");
-	}
-	else if (sig == SIGQUIT)
-	{
-		printf("Ctrl + \\ (SIGQUIT)\n");
-	}
+	(void)sig;
+	printf("\n");
+	rl_on_new_line();
+    rl_replace_line("", 1);
+    rl_redisplay();
 }
 
 void	register_sig_handler()
 {
-	signal(SIGINT, sig_handle);
-	signal(SIGTERM, sig_handle);
-	signal(SIGQUIT, sig_handle);
+	signal(SIGINT, sig_int_handle);
+	signal(SIGQUIT, SIG_IGN);
 }
