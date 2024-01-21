d52496a9077751cb83da08a0b3c1aaa27ec9f4ef
yonyoo <yonyoo@student.42seoul.kr>
2024-01-21 16:51:27 +0900

    Update signal_handle : Fix Norminette

--- sig_handle/sig_handle.c
+++ sig_handle/sig_handle.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 20:21:47 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/01/21 16:31:18 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/01/21 16:51:11 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ static void	sig_int_handle(int sig)
 	(void)sig;
 	printf("\n");
 	rl_on_new_line();
-    rl_replace_line("", 1);
-    rl_redisplay();
+	rl_replace_line("", 1);
+	rl_redisplay();
 }
 
-void	register_sig_handler()
+void	register_sig_handler(void)
 {
 	signal(SIGINT, sig_int_handle);
 	signal(SIGQUIT, SIG_IGN);
--- sig_handle/sig_handle.h
+++ sig_handle/sig_handle.h
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 20:21:17 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/01/21 16:31:23 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/01/21 16:51:16 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 # define SIG_HANDLE_H
 # include "../pm_shell.h"
 
-void	register_sig_handler();
+void	register_sig_handler(void);
 
 #endif
