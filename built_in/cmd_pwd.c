c5b571bfc8dc349805a9b7203a62e83ee3bcfc25
yonyoo <yonyoo@student.42seoul.kr>
2024-01-26 01:39:07 +0900

    Update Built-In pwd : Imoproved Logic

--- built_in/cmd_pwd.c
+++ built_in/cmd_pwd.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 16:28:46 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/01/21 16:43:55 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/01/26 01:38:48 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 int	cmd_pwd(void)
 {
-	char	cwd[PATH_MAX];
+	char	*cwd;
 
-	if (getcwd (cwd, PATH_MAX))
-	{
-		printf("%s\n", cwd);
-		return (1);
-	}
-	else
+	cwd = getcwd (NULL, 0);
+	if (!cwd)
 		return (0);
+	printf("%s\n", cwd);
+	free(cwd);
+	return (1);
 }
