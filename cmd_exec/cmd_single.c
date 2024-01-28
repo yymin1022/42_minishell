1c4700a383c714081a3c7e9ee5aae558fd05b963
yonyoo <yonyoo@student.42seoul.kr>
2024-01-29 06:11:11 +0900

    Update cmd_exec : Added Single/Multiple Codes

--- cmd_exec/cmd_single.c
+++ cmd_exec/cmd_single.c
@@ -6,11 +6,13 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 05:46:11 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/01/29 06:08:02 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/01/29 06:08:56 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cmd_exec.h"
+
+extern int	g_status_code;
 
 static int	exec_child_cmd(t_cmd *cmd)
 {
@@ -40,4 +42,5 @@ void	exec_single_cmd(t_cmd *cmd, t_env *env_list)
 		exit_code = exec_builtin(cmd->argv, env_list);
 	else
 		exit_code = exec_child_cmd(cmd);
+	g_status_code = exit_code;
 }
