838f0ba23f9b029f9570401627ed86272cc1acaa
falconlee236 <falconlee236@cau.ac.kr>
2024-01-29 10:29:21 +0900

    add handle_env_in_heredoc

--- cmd_exec/cmd_heredoc.c
+++ cmd_exec/cmd_heredoc.c
@@ -6,13 +6,32 @@
 /*   By: isang-yun <isang-yun@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 10:18:17 by isang-yun         #+#    #+#             */
-/*   Updated: 2024/01/29 10:19:03 by isang-yun        ###   ########.fr       */
+/*   Updated: 2024/01/29 10:29:05 by isang-yun        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cmd_exec.h"
 
 extern int	g_status_code;
+
+static void	handle_env_in_heredoc(char *input, int fd, t_env *env)
+{
+	int		start;
+	int		end;
+	char	*env_value;
+
+	start = 0;
+	end = 0;
+	while (input[end])
+	{
+		if (input[end] == '$')
+		{
+			write(fd, input + start, end - start);
+			start = end;
+			// env_value = 
+		}
+	}
+}
 
 static void	create_tmpfile(char *file_name, char *delimiter, t_env *env_list)
 {
@@ -27,12 +46,13 @@ static void	create_tmpfile(char *file_name, char *delimiter, t_env *env_list)
 		input = readline("heredoc> ");
 		if (!input)
 			ft_putstr_fd("\033[1A\033[2C", STDERR_FILENO);
-		if (!input || (ft_strncmp(input, delimiter, ft_strlen(input)) == 0)
-			&& (ft_strlen(input) == ft_strlen(delimiter)))
+		if (!input || (ft_strncmp(input, delimiter, ft_strlen(input)) == 0
+				&& ft_strlen(input) == ft_strlen(delimiter)))
 		{
 			close(fd);
 			return ;
 		}
+		handle_env_in_heredoc(input, fd, env_list);
 		write(fd, "\n", 1);
 		free(input);
 	}
@@ -40,7 +60,7 @@ static void	create_tmpfile(char *file_name, char *delimiter, t_env *env_list)
 	free(input);
 }
 
-static void	exec_heredoc(t_cmd *cmd_list, t_env *env_list)
+void	exec_heredoc(t_cmd *cmd_list, t_env *env_list)
 {
 	int			file_cnt;
 	char		*file_str;
@@ -54,7 +74,7 @@ static void	exec_heredoc(t_cmd *cmd_list, t_env *env_list)
 		while (cur_redirect)
 		{
 			if (ft_strncmp(cur_redirect->type, "<<", 2)
-				&& ft_strlen(cur_redirect) == 2)
+				&& ft_strlen(cur_redirect->type) == 2)
 			{
 				file_str = ft_itoa(file_cnt);
 				file_name = ft_strjoin(file_str, ".tmp");
