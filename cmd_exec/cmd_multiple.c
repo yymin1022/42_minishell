9b145778827cdce4f74fe2a45117df35f0ab9436
yonyoo <yonyoo@student.42seoul.kr>
2024-02-04 21:05:16 +0900

    Update cmd_exec : Added Pipe Redirection Logic

--- cmd_exec/cmd_multiple.c
+++ cmd_exec/cmd_multiple.c
@@ -6,15 +6,55 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 05:46:35 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/01/29 06:14:20 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/02/04 21:05:33 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cmd_exec.h"
 
+static void	exec_child(t_cmd *cmd, t_env *env_list, int last_process)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	close(cmd->pipe[0]);
+	if (last_process != 0 && dup2(cmd->pipe[1], 1) == -1)
+		perror("dup2(pipe_write)");
+	close(cmd->pipe[1]);
+	exec_command(cmd, env_list);
+}
+
+static void	exec_parent(t_cmd *cmd)
+{
+	close(cmd->pipe[1]);
+	if (dup2(cmd->pipe[0], 0) == -1)
+		perror("dup2(stdin)");
+	close(cmd->pipe[0]);
+}
+
 void	exec_multiple_cmd(t_cmd *cmd, t_env *env_list, int cmd_cnt)
 {
+	pid_t	pid;
+	int		idx;
+
 	(void)cmd;
 	(void)cmd_cnt;
 	(void)env_list;
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+	idx = 1;
+	while (cmd)
+	{
+		if (pipe(cmd->pipe) == -1)
+			exit_msg("Pipe Error");
+		pid = fork();
+		if (pid == -1)
+			exit_msg("Fork Error");
+		else if (pid == 0)
+			exec_child(cmd, env_list, cmd_cnt - idx);
+		else
+			exec_parent(cmd);
+		cmd = cmd->next;
+		idx++;
+	}
+	cmd_wait_child(pid, cmd_cnt);
 }
