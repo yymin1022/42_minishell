9a155ad8d0cbc963707df3f7e1cb539b0606730f
yonyoo <yonyoo@student.42seoul.kr>
2024-02-04 21:14:14 +0900

    Update cmd_exec : Added Signal Restoration After Exec

--- cmd_exec/cmd_exec.c
+++ cmd_exec/cmd_exec.c
@@ -6,11 +6,22 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 16:39:09 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/02/03 02:23:22 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/02/04 21:13:59 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cmd_exec.h"
+
+static void	reset_fd_signal(int stdin_fd, int stdout_fd)
+{
+	if (dup2(stdin_fd, 0) == -1)
+		exit_msg("FD Error");
+	if (dup2(stdout_fd, 1) == -1)
+		exit_msg("FD Error");
+	close(stdin_fd);
+	close(stdout_fd);
+	init_sig_handler();
+}
 
 static int	get_cmd_cnt(t_cmd *cmd_list)
 {
@@ -28,12 +39,17 @@ static int	get_cmd_cnt(t_cmd *cmd_list)
 void	exec_cmd_list(t_cmd *cmd_list, t_env *env_list)
 {
 	int	cmd_cnt;
+	int	stdin_fd;
+	int	stdout_fd;
 
 	cmd_cnt = get_cmd_cnt(cmd_list);
+	stdin_fd = dup(0);
+	stdout_fd = dup(1);
 	exec_heredoc(cmd_list, env_list);
 	if (get_cmd_cnt(cmd_list) > 1)
 		exec_multiple_cmd(cmd_list, env_list, cmd_cnt);
 	else
 		exec_single_cmd(cmd_list, env_list);
 	unlink_heredoc_tmp(cmd_list);
+	reset_fd_signal(stdin_fd, stdout_fd);
 }
