920f405e28ca9630b315d4bbbda19f5fb68e28c0
yonyoo <yonyoo@student.42seoul.kr>
2024-02-04 21:05:05 +0900

    Update cmd_exec : Added Pipe Redirection Logic

---
+++ cmd_exec/cmd_redirect.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_redirect.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/04 19:58:11 by yonyoo            #+#    #+#             */
+/*   Updated: 2024/02/04 20:05:32 by yonyoo           ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cmd_exec.h"
+
+// int	redirect_io(t_cmd *cmd, int is_child)
+// {
+// 	int			status;
+// 	t_redirect	*redir;
+
+// 	if (cmd->redirect == NULL)
+// 		return (1);
+// 	redir = cmd->redirect;
+// 	status = 1;
+// 	while (redir && status)
+// 	{
+// 		// if (redir->type == )
+// 	}
+// }
